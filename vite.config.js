c96d1efef85be8fbc424489ecee6bdd0f2d49c5d
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2025-09-12 15:07:03 -0300

    adding location.html in build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/proyecto-3-modern-art-gallery",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        location: "./location.html",
+      },
+    },
+  },
 });
